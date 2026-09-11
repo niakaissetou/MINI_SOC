2102ac4a4bb007608c6d8dddcc6534b69008b66e
Aissetou NIAKATE <114932145+niakaissetou@users.noreply.github.com>
2026-09-11 19:27:11 +0200

    Refactor main.py for clarity and consistency

--- py_file/main.py
+++ py_file/main.py
@@ -1,24 +1,20 @@
 import os
 import re
 
-# Aller à la racine
 os.chdir("/")
-file_path = os.path.join("var", "log", "auth.log") #crée le chemin depuis la racine
+file_path = os.path.join("var", "log", "auth.log")
 failed_count = 0
 ip_list = []
 
 with open(file_path, "r") as file:
 
-    logs = file.readlines()  # liste de lignes des logs
+    logs = file.readlines()
 
     for line in logs:
 
-        #{1,3} = entre 1 et 3 chiffres, \b -> pas de combinaisons bizarres et \. pour la forme d'un ip
-
         if ("Failed password") in line:
-            ip_list.append(re.findall(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', line)) #\d = un chiffre (0–9)
+            ip_list.append(re.findall(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', line)) #\d = un chiffre entre 0 et 9
 
-            #print(line.strip())
             failed_count += 1
 
 if failed_count == 0:
@@ -32,4 +28,4 @@ else :
             print(f"ip that failed to connect is ->{ip}")
 
 
-#Pour tester -> fakeusr@localhost ou l'adresse de loopback a la place de localhost
+#Pour tester --> fakeusr@localhost ou l'adresse de loopback a la place de localhost
